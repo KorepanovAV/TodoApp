98f9d9c2b8c3b20dd762b2b4c19cb68584c91797
KorepanovAV <korepanov_av@directum.ru>
2023-12-14 18:48:25 +0400

    Исправил путь прокси.

--- todoapp.client/vite.config.ts
+++ todoapp.client/vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/weatherforecast': {
-                target: 'https://localhost:5001/',
+                target: 'http://localhost:5169/',
                 secure: false
             }
         },
