325b7df41b820869d1bb9f666b1a23133c5c93d0
KorepanovAV <korepanov_av@directum.ru>
2023-12-19 22:00:41 +0400

    + подключил bem.

---
+++ todoapp.client/postcss.config.cjs
@@ -0,0 +1,20 @@
+﻿module.exports = {
+  plugins: [
+    require('postcss-import')({ /* ...options */ }),
+    require('postcss-each')({ /* ...options */ }),
+    require('postcss-bem-fix')({
+      style: 'bem',
+      separators: {
+        descendent: '__'
+      },
+      shortcuts: {
+        component: 'b',
+        descendent: 'e',
+        modifier: 'm'
+      }
+    }),
+    require('postcss-nested')({ /* ...options */ }),
+    //require('autoprefixer')({ /* ...options */ }),
+    require('postcss-discard-comments')
+  ]
+};
