1f069d77a845cd64b0e62dd525976ae7446ca35b
KorepanovAV <korepanov_av@directum.ru>
2023-12-22 16:32:19 +0400

    * Доработал todo-item & todo-list.

--- todoapp.client/src/components/todo-list/todo-list.tsx
+++ todoapp.client/src/components/todo-list/todo-list.tsx
@@ -1,8 +1,11 @@
 import { ITodo } from "../ITodo";
 import { ITodoActions } from "../ITodoActions";
 import { TodoItem } from "../todo-item/todo-item";
+import block from 'bem-cn-lite';
 
 import './todo-list.css';
+
+const b = block('todo-list');
 
 interface ITodoListProps {
     todos: ITodo[];
@@ -16,11 +19,11 @@ export function TodoList(props: ITodoListProps) {
     const content = todos === undefined
         ? <p><em>Loading...</em></p>
         :
-        <div>
+        <div className={b()}>
             {todos
                 .sort(todoCompare)
                 .map(todo =>
-                    <TodoItem key={todo.id} todo={todo} todoActions={todoActions} />
+                    <TodoItem key={todo.id} {...{ todo, todoActions, className: b('item') }} />
                 )}
         </div>;
 
